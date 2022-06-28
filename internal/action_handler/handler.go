1e775145b5ec9541cb42a3cc078858d98ce75ccf
dengzii <dengzii@foxmail.com>
2022-06-28 15:59:50 +0800

    add heartbeat handler

--- internal/action_handler/handler.go
+++ internal/action_handler/handler.go
@@ -1,7 +1,11 @@
 package action_handler
 
-import "github.com/glide-im/glide/pkg/messaging"
+import (
+	"github.com/glide-im/glide/pkg/messages"
+	"github.com/glide-im/glide/pkg/messaging"
+)
 
 func Setup(handler messaging.Interface) {
 	handler.AddHandler(&InternalActionHandler{})
+	handler.AddHandler(messaging.NewActionHandler(messages.ActionHeartbeat, handleHeartbeat))
 }
