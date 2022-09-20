a1eb2f96e4db597cb83c8eb9cdde527832f453a1
dengzii <dengzii@foxmail.com>
2022-09-20 17:26:46 +0800

    update doc

--- example/client/rpc_client_example.go
+++ example/client/rpc_client_example.go
@@ -9,22 +9,37 @@ import (
 )
 
 func main() {
+	// 消息网关
 	//RpcGatewayClientExample()
+
+	// 发布订阅(群聊)
 	RpcSubscriberClientExample()
 }
 
 func RpcGatewayClientExample() {
+
+	// 消息网关配置
 	options := &rpc.ClientOptions{
 		Addr: "127.0.0.1",
 		Port: 8092,
 		Name: "im_rpc_server",
 	}
+
+	// 创建消息网关接口客户端
 	cli, err := client.NewGatewayRpcImpl(options)
 	defer cli.Close()
 	if err != nil {
 		panic(err)
 	}
+
+	// 给网关中指定 id 的链接推送一条消息
 	err = cli.EnqueueMessage(gate.NewID2("1"), messages.NewEmptyMessage())
+	if err != nil {
+		panic(err)
+	}
+
+	// 设置网关中连接新 id
+	err = cli.SetClientID(gate.NewID2("1"), gate.NewID2("2"))
 	if err != nil {
 		panic(err)
 	}
